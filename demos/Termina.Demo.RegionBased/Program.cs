1593fc4020564cb991611bcfc160243cdb37ef60
Aaron Stannard <aaron@petabridge.com>
2025-12-15 19:06:39 -0600

    Move demos to dedicated folder and rename V2 to RegionBased (#36)
    * Move demos to dedicated folder and rename V2 to RegionBased
    
    - Move all demo projects from src/ to demos/ folder
    - Rename Termina.Demo.V2 to Termina.Demo.RegionBased for clarity
    - Update namespace from Termina.Demo.V2 to Termina.Demo.RegionBased
    - Update solution file with new project paths
    - Update project references to use new relative paths
    - Add keyboard hint instructions to RegionBased demo status bar
    
    * Update CI workflow for demos folder location

--- demos/Termina.Demo.RegionBased/Program.cs
+++ demos/Termina.Demo.RegionBased/Program.cs
@@ -2,7 +2,7 @@
 // Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
 
 using Microsoft.Extensions.Hosting;
-using Termina.Demo.V2;
+using Termina.Demo.RegionBased;
 using Termina.Hosting;
 using Termina.Input;
 
